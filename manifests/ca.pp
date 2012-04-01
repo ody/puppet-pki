43af47946095bfb9adc6dfa5f9e3a3d006e8742e
Zach Leslie <zach@puppetlabs.com>
2012-03-31 21:06:48 -0700

    apply better ordering

--- manifests/ca.pp
+++ manifests/ca.pp
@@ -7,35 +7,42 @@ define easyrsa::ca (
   $key_province = 'OR',
   $key_city     = "Portland",
   $key_email    = "admin@foo.bar",
-  $key_org      = "TechOps",
-  $key_cn       = "changeme",
-  $key_name     = "changeme",
-  $key_ou       = "changeme",
+  $key_org      = "Acme",
+  $key_name     = "Root",
+  $key_ou       = "TechOps",
   $base_dir
 ) {
 
-  easyrsa { $title:
-    base_dir => $base_dir,
-    before   => Pkitool["Generate CA at ${base_dir}"],
-  }
+  $key_cn = $name
+
+  easyrsa { $title: base_dir => $base_dir, }
 
   file { "${base_dir}/vars":
     mode    => 700,
     content => template("easyrsa/vars.erb"),
     before  => Pkitool["Generate CA at ${base_dir}"],
+    require => Easyrsa[$title],
   }
 
   exec { "Clean All at ${base_dir}":
-    cwd         => $base_dir,
-    command     => "/bin/bash -c \"(source $base_dir/vars > /dev/null; ${base_dir}/clean-all)\"",
-    creates     => "${base_dir}/keys/serial",
-    before      => Pkitool["Generate CA at ${base_dir}"],
+    cwd     => $base_dir,
+    command => "/bin/bash -c \"(source $base_dir/vars > /dev/null; ${base_dir}/clean-all)\"",
+    creates => "${base_dir}/keys/serial",
+    require => [ Easyrsa[$title], File["${base_dir}/vars"] ],
   }
 
   Pkitool { base_dir => $base_dir }
   pkitool { "Generate CA at ${base_dir}":
     command => "--initca",
     creates => "${base_dir}/keys/ca.crt",
+    require => Exec["Clean All at ${base_dir}"],
+  }
+
+  exec { "Build DH at ${base_dir}":
+    cwd     => $base_dir,
+    command => "/bin/bash -c \"(source $base_dir/vars > /dev/null; ${base_dir}/build-dh ${key_size})\"",
+    creates => "${base_dir}/keys/dh${key_size}.pem",
+    require => Pkitool["Generate CA at ${base_dir}"],
   }
 
 }
